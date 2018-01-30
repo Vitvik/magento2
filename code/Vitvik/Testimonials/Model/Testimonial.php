8a1cd2739156492f8f892e30242ebfd94dbfa0a9
Vitvik <tkachukvitaliy@gmail.com>
2018-01-30 09:00:18 +0200

    adminhtml grid

--- code/Vitvik/Testimonials/Model/Testimonial.php
+++ code/Vitvik/Testimonials/Model/Testimonial.php
@@ -3,14 +3,84 @@ namespace Vitvik\Testimonials\Model;
 class Testimonial extends \Magento\Framework\Model\AbstractModel implements TestimonialInterface, \Magento\Framework\DataObject\IdentityInterface
 {
     const CACHE_TAG = 'testimonials';
+    protected $_cacheTag= 'testimonials';
+    protected $_eventPrefix = 'testimonials';
     protected function _construct()
     {
         $this->_init('Vitvik\Testimonials\Model\ResourceModel\Testimonial');
     }
+    /*
     public function getIdentities()
     {
         return [self::CACHE_TAG . '_' . $this->getId()];
     }
+*/
+    //
+    public function getTestimonialId()
+    {
+        return $this->getData(self::TESTIMONIAL_ID);
+    }
+
+    public function setTestimonialId($testimonialId)
+    {
+        return $this->setData(self::TESTIMONIAL_ID, $testimonialId);
+    }
+
+
+    public function getUserId()
+    {
+        return $this->getData(self::USER_ID);
+    }
+
+    public function setUserId($userId)
+    {
+        return $this->setData(self::USER_ID, $userId);
+    }
+
+
+    public function getTestimonialContent()
+    {
+        return $this->getData(self::TESTIMONIAL_CONTENT);
+    }
+
+    public function setTestimonialContent($testimonialContent)
+    {
+        return $this->setData(self::TESTIMONIAL_CONTENT, $testimonialContent);
+    }
+
+
+    public function getIsActive()
+    {
+        return $this->getData(self::IS_ACTIVE);
+    }
+
+    public function setIsActive($isActive)
+    {
+        return $this->setData(self::IS_ACTIVE, $isActive);
+    }
+
+
+    public function getUpdateTime()
+    {
+        return $this->getData(self::UPDATE_TIME);
+    }
+
+    public function setUpdateTime($updateTime)
+    {
+        return $this->setData(self::UPDATE_TIME, $updateTime);
+    }
+
+
+    public function getCreatedAt()
+    {
+        return $this->getData(self::CREATED_AT);
+    }
+
+    public function setCreatedAt($createdAt)
+    {
+        return $this->setData(self::CREATED_AT, $createdAt);
+    }
+    //
 }
 
 
--- code/Vitvik/Testimonials/Model/TestimonialInterface.php
+++ code/Vitvik/Testimonials/Model/TestimonialInterface.php
@@ -2,5 +2,41 @@
 namespace Vitvik\Testimonials\Model;
 interface testimonialInterface
 {
+    const TESTIMONIAL_ID = 'testimonial_id';
+    const USER_ID = 'user_id';
+    const TESTIMONIAL_CONTENT = 'testimonial_content';
+    const IS_ACTIVE = 'is_active';
+    const UPDATE_TIME = 'update_time';
+    const CREATED_AT = 'created_at';
+
+
+    public function getTestimonialId();
+
+    public function setTestimonialId($entityId);
+
+
+    public function getUserId();
+
+    public function setUserId($userId);
+
+
+    public function getTestimonialContent();
+
+    public function setTestimonialContent();
+
+
+    public function getIsActive();
+
+    public function setIsActive($isActive);
+
+
+    public function getUpdateTime();
+
+    public function setUpdateTime($updateTime);
+
+
+    public function getCreatedAt();
+
+    public function setCreatedAt($createdAt);
 
 }
